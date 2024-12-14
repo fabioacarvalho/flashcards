1dc0f658d10754f41c3b9197d9c5fbeccafa572b
Fabio-Carvalho <fabio22191@hotmail.com>
2024-12-14 17:33:18 -0300

    setup environment

---
+++ frontend/setup/src/components/Header.tsx
@@ -0,0 +1,9 @@
+import styles from "../app/page.module.css";
+
+export const Header = () => {
+    return (
+        <header className={styles.header}>
+            <h1 className={styles.title}>Flashcards</h1>
+        </header>
+    );
+}
