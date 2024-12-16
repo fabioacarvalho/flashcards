b529e8fb1aac0a9c1a1ddf05e07da6e1dc912908
Fabio-Carvalho <fabio22191@hotmail.com>
2024-12-16 02:35:30 -0300

    setup backend and first model

---
+++ backend/api/services/card_service.py
@@ -0,0 +1,19 @@
+from ..models import card_model
+from api import db
+
+
+def add_card(card):
+    card_db = card_model.Card(
+        lastLevel=card.lastLevel, 
+        front=card.front, 
+        back=card.back, 
+        daysLastView=card.daysLastView, 
+        study=card.study,  # Este campo agora é opcional
+        content=card.content,
+        picture=card.picture,
+        updatedAt=card.updatedAt,
+        nextView=card.nextView
+    )
+    db.session.add(card_db)
+    db.session.commit()
+    return card_db
---
+++ backend/api/views/card_view.py
@@ -0,0 +1,38 @@
+from flask_restful import Resource
+from ..schemas import card_schema
+from flask import request, make_response, jsonify
+from ..entidades import card
+from ..services import card_service
+from api import api
+
+
+class CardList(Resource):
+    
+    def post(self):
+        cs = card_schema.CardSchema()
+        validate = cs.validate(request.json)
+
+        if validate:
+            return make_response(jsonify(validate), 400)
+        else:
+            lastLevel = request.json["lastLevel"]
+            front = request.json["front"]
+            back = request.json["back"]
+            content = request.json["content"]
+            picture = request.json["picture"]
+            daysLastView = request.json["daysLastView"]
+            updatedAt = request.json["updatedAt"]
+            nextView = request.json["nextView"]
+            study = request.json["study"]
+
+            new_card = card.Card(lastLevel=lastLevel, front=front, back=back, content=content, picture=picture, daysLastView=daysLastView, updatedAt=updatedAt, nextView=nextView, study=study)
+
+            result = card_service.add_card(new_card)
+            return make_response(cs.jsonify(result), 201)
+
+
+class CardDetail(Resource):
+    pass
+
+
+api.add_resource(CardList, '/add/card')
