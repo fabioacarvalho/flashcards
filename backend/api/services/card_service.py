6489ddd6f18dca1258f35e00df44be247dede1f6
Fabio-Carvalho <fabio22191@hotmail.com>
2024-12-16 03:14:39 -0300

    created method PUT and DELETE

--- backend/api/services/card_service.py
+++ backend/api/services/card_service.py
@@ -22,3 +22,29 @@ def add_card(card):
 def list_cards():
     cards = card_model.Card.query.all()
     return cards
+
+
+def get_card_id(id):
+    card = card_model.Card.query.filter_by(id=id).first()
+    return card
+
+
+def delete_card(card):
+    db.session.delete(card)
+    db.session.commit()
+
+
+def update_card(current_card, edited_card):
+
+    current_card.lastLevel = edited_card.lastLevel 
+    current_card.front = edited_card.front 
+    current_card.back = edited_card.back 
+    current_card.daysLastView = edited_card.daysLastView 
+    current_card.study = edited_card.study
+    current_card.content = edited_card.content
+    current_card.picture = edited_card.picture
+    current_card.updatedAt = edited_card.updatedAt
+    current_card.nextView = edited_card.nextView
+    
+    db.session.commit()
+    return current_card
--- backend/api/views/card_view.py
+++ backend/api/views/card_view.py
@@ -36,7 +36,48 @@ class CardList(Resource):
 
 
 class CardDetail(Resource):
-    pass
+    def get(self, id):
+        card = card_service.get_card_id(id)
+        if card is None:
+            return make_response(jsonify("Card not found"), 404)
+        
+        cs = card_schema.CardSchema()
+        return make_response(cs.jsonify(card), 200)
 
+    def delete(self, id):
+        card = card_service.get_card_id(id)
+        if card is None:
+            return make_response(jsonify("Card not found"), 404)
+        
+        card_service.delete_card(card)
+        return make_response('', 204)
+    
+    def put(self, id):
+        current_card = card_service.get_card_id(id)
+        if current_card is None:
+            return make_response(jsonify("Card not found"), 404)
+        
+        cs = card_schema.CardSchema()
+        validate = cs.validate(request.json)
 
+        if validate:
+            return make_response(jsonify(validate), 400)
+        else:
+            lastLevel = request.json["lastLevel"]
+            front = request.json["front"]
+            back = request.json["back"]
+            content = request.json["content"]
+            picture = request.json["picture"]
+            daysLastView = request.json["daysLastView"]
+            updatedAt = request.json["updatedAt"]
+            nextView = request.json["nextView"]
+            study = request.json["study"]
+
+            edited_card = card.Card(lastLevel=lastLevel, front=front, back=back, content=content, picture=picture, daysLastView=daysLastView, updatedAt=updatedAt, nextView=nextView, study=study)
+
+            result = card_service.update_card(current_card, edited_card)
+            return make_response(cs.jsonify(result), 201)
+    
+    
 api.add_resource(CardList, '/cards')
+api.add_resource(CardDetail, '/card/<int:id>')
